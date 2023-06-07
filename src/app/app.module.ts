18d8c69e7e754e336ab4aa8c3f12035117942704
preetbista <bistapreet4@gmail.com>
2023-06-07 15:58:08 +0545

    remove the unnecessary imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './user/header/header.component';
-import { FooterComponent } from './user/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModuleModule } from './shared-module/shared-module.module';
 import { AdminModule } from './admin/admin.module';
@@ -33,8 +31,6 @@ import { MatIcon } from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent
   ],
   imports: [
     BrowserModule,
