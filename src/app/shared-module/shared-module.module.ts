4d6ae39675b323b3c9818ca86365889f97202752
preetbista <bistapreet4@gmail.com>
2023-06-07 15:56:54 +0545

    minor changes in shared module

--- src/app/shared-module/shared-module.module.ts
+++ src/app/shared-module/shared-module.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { HeaderComponent } from '../user/header/header.component';
-import { FooterComponent } from '../user/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     PageNotFoundComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
   ]
 
 })
